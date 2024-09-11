b10514720cfbfd66e7ce1ec614350bc9c0f38152
MiguelAn05 <miguelingil89@gmail.com>
2024-09-11 15:56:28 -0500

    people

---
+++ Assets/Scripts/Finishing.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finishing : MonoBehaviour
+{
+   [SerializeField] private GameObject meta;
+
+   private void OnTriggerEnter(Collider other)
+   {
+      if (other.CompareTag("Player"))
+      {
+         meta.SetActive(true);
+      }
+   }
+
+   private void OnTriggerExit(Collider other)
+   {
+      if (other.CompareTag("Player"))
+      {
+         meta.SetActive(false);
+      }
+   }
+}
